b4362b35fee48bad2b4d7d89fd4d88f91cacc2dd
TI Sabbir <tisabbir0@gmail.com>
2024-08-27 23:15:58 +0600

    Heading added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Heading from "@/components/Heading";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       Hello Next Hero
+
+      <Heading />
     </main>
   );
 }
---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            This is my heading
+        </div>
+    );
+};
+
+export default Heading;
