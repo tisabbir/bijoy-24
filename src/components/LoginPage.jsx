978ac3e599c501d7300c8d0e5c9ac4f6566d5b6d
TI Sabbir <tisabbir0@gmail.com>
2024-08-30 16:31:26 +0600

    modify : login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,10 +4,7 @@ import React from 'react';
 const LoginPage = () => {
     return (
         <div>
-            This is login page
-
-            <Link>Login</Link>
-            <Link onClick={'/register'} >register</Link>
+            
         </div>
     );
 };
